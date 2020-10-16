4aa5582ecea315ee8f010a765d8345ad0473d950
renderVis <rendervis333@gmail.com>
2020-10-16 17:26:07 +0300

    updated .json file for backend

--- backend/bin/server.js
+++ backend/bin/server.js
@@ -1,4 +1,4 @@
 const app = require("../app");
-const port = 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`listening on port ${port}`));
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));

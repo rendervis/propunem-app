86c4c7e1a698e8456a95f40fe6277d9d3477b9be
renderVis <rendervis333@gmail.com>
2020-10-17 09:51:15 +0300

    config for prod-heroku

---
+++ environment_config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  APP_SECRET: process.env.APP_SECRET,
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  poolConfiguration: process.env.poolConfiguration,
+};

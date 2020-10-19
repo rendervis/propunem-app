cbc842be79761445d11770ae89f8ce6e7fee59b8
renderVis <rendervis333@gmail.com>
2020-10-19 14:49:46 +0300

    proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -19,12 +19,12 @@ module.exports = function (app) {
       target: "http://localhost:5000",
     })
   );
-  app.use(
-    "/api/*",
-    createProxyMiddleware({
-      target: "http://localhost:5000",
-    })
-  );
+  // app.use(
+  //   "/api/*",
+  //   createProxyMiddleware({
+  //     target: "http://localhost:5000",
+  //   })
+  // );
   app.use(
     "/api/account/*",
     createProxyMiddleware({

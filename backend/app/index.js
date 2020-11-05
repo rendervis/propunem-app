666106bdc3565caccb4295f1c1dbebf0e50aff47
renderVis <rendervis333@gmail.com>
2020-11-05 09:39:18 +0200

    updated index.html path

--- backend/app/index.js
+++ backend/app/index.js
@@ -103,8 +103,8 @@ if (isProduction) {
   const path = require("path");
   app.use(express.static(path.join(__dirname, "../../client/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../client/build"));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../client/build", "index.html"));
   });
 }
 

f75d731fe086229c0b6af417583078d423ddcf28
renderVis <rendervis333@gmail.com>
2020-11-18 10:03:03 +0200

    fixed bug on ServicesAndCapabilities --> not showing the proposal name

--- client/src/containers/pages/proposal/ServicesAndCapabilities.js
+++ client/src/containers/pages/proposal/ServicesAndCapabilities.js
@@ -1,44 +1,60 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { v4 as uuidv4, parse as uuidParse } from "uuid";
-
-///////COMPONENTS
-import ProposalList from "../profile/ProposalList";
+import styled, { css } from "styled-components";
 
 ///////UI
 import { TextSmall } from "../../../components/UI/ui-elements";
-//////actions
-import {
-  createText,
-  fetchAboutUsText,
-  saveText,
-  deleteText,
-  showDefault,
-  aboutUsClearState,
-  updateTouched,
-} from "../../../redux/actions/about-us.actions";
 
 const ServicesAndCapabilities = (props) => {
-  const [newValue, setNewValue] = useState("");
-
-  const dispatch = useDispatch();
-  const proposalId =
-    useSelector((state) => state.proposal.proposalId) ||
-    props.match.params.proposalId;
-
-  let text =
+  const proposalList = useSelector((state) => state.proposal.proposalList);
+  const brandingText =
     useSelector((state) => state.branding.brandingDeclarationDB[1].text) ||
     "Declaratia ta de branding vine aici!";
-  // console.log("text", text);
+
+  const renderNameList = (props) => {
+    if (!proposalList) {
+      return (
+        <span
+          style={{
+            paddingTop: "1.6rem",
+            paddingBottom: "1.38rem",
+            marginLeft: "5.44rem",
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          Nu ai propuneri!
+        </span>
+      );
+    }
+    return proposalList.map((name) => {
+      return <StyledSpan {...props}>{name.proposal_name}</StyledSpan>;
+    });
+  };
 
   return (
     <div>
       <div style={{ fontSize: "8px" }}>BRANDING DECLARATION</div>
-      <div style={{ marginTop: "16px" }}>{text}</div>
+      <div style={{ marginTop: "16px" }}>{brandingText}</div>
 
-      <ProposalList noClick="none" />
+      <div
+        style={{
+          paddingTop: "1.6rem",
+          paddingBottom: "1.38rem",
+          // marginLeft: "5.44rem",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        {renderNameList()}
+      </div>
     </div>
   );
 };
+
+const StyledSpan = styled.span`
+  margin-bottom: 10px;
+`;
 
 export default ServicesAndCapabilities;
